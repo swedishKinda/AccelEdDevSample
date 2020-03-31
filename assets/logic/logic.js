46d84fd3d89ead63838c10b1fd09b6c3282ed382
chris284891 <chris284891@gmail.com>
2020-03-31 15:02:10 -0400

    linked images that will be used in slide show

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,1 +1,18 @@
-const foo = "foo"
+// const foo = "foo"
+
+$(document).ready(function () {
+    // set up DOM manipulation variables:
+    var displayCarousel = $(".display-slick");
+    displayCarousel.show();
+})
+
+
+// var carouselDiv = $(".display-slick");
+
+// carouselDiv.slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+// });
+
+// carouselDiv.show();
